0cdf3072965b4923524f0459211dc229f1698348
Ajay Rajput <ajayrajpoot.cs@gmail.com>
2024-03-12 01:33:50 +0530

    Fixed ViewResolver

--- src/main/java/com/springmvc/controller/HomeController.java
+++ src/main/java/com/springmvc/controller/HomeController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-	@ResponseBody
 	@RequestMapping("/")
 	public String message()
 	{
 		System.out.println("Suceess.....");
-		return "index.jsp";
+		return "index";
 	}
 
 }
